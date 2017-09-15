b195c6c432e2a7af75478c1aa79c2b35d4d26f54
韩亚飞_韩梦飞沙 <313134555@qq.com>
2017-09-16 02:17:45 +0800

    -

---
+++ gen/com/example/ecodetest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.ecodetest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/ecodetest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ecodetest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ecodeingView ev=new ecodeingView(this);
+        setContentView(ev);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
